7234953d8c31bf68628976b9fffac89748ad21a5
alessandrofelder <alessandrofelder@users.noreply.github.com>
2024-01-24 17:38:25 +0000

    implement a layerlist that keeps up with core add/delete layer

--- src/napari_experimental/_widget.py
+++ src/napari_experimental/_widget.py
@@ -3,16 +3,14 @@ This module contains an experimental implementation of a layers widget.
 """
 from typing import TYPE_CHECKING
 
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QWidget
 
 if TYPE_CHECKING:
     import napari
 
 
-import numpy as np
 from napari._qt.containers import QtLayerList
 from napari.components import LayerList
-from napari.layers import Image
 
 
 class LayersWidget(QWidget):
@@ -21,16 +19,18 @@ class LayersWidget(QWidget):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__()
 
-        add_layer_button = QPushButton("Add a random layer")
-        self.layer_list = LayerList()
+        self.viewer = viewer
+        self.viewer.layers.events.inserted.connect(self._on_add_layer)
+        self.viewer.layers.events.removed.connect(self._on_remove_layer)
 
+        self.layer_list = LayerList()
         qt_layer_list = QtLayerList(root=self.layer_list, parent=self)
+
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(qt_layer_list)
-        qt_layer_list.setVisible(True)
-        self.layout().addWidget(add_layer_button)
-        add_layer_button.clicked.connect(self._on_click)
 
-    def _on_click(self):
-        random_image_layer = Image(np.random.random((10, 10)))
-        self.layer_list.append(random_image_layer)
+    def _on_add_layer(self, event):
+        self.layer_list.append(event.value)
+
+    def _on_remove_layer(self, event):
+        self.layer_list.remove(event.value)
