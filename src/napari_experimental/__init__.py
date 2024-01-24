fd072c5159b841cd58fe96580f37bf405a052369
alessandrofelder <alessandrofelder@users.noreply.github.com>
2024-01-24 16:51:08 +0000

    modify manifest (and related) to have just LayersWidget

--- src/napari_experimental/__init__.py
+++ src/napari_experimental/__init__.py
@@ -1,14 +1,4 @@
 __version__ = "0.0.1"
-from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
-)
+from ._widget import LayersWidget
 
-__all__ = (
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-)
+__all__ = ("LayersWidget",)
