569b1559126efb1e82a89bb5966edde2a71437f2
willGraham01 <ccaegra@ucl.ac.uk>
2024-06-19 09:12:34 +0100

    Rename single test

--- tests/test_group_layer_widget.py
+++ tests/test_group_layer_widget.py
@@ -2,6 +2,6 @@ from napari_experimental._widget import GroupLayerWidget
 from qtpy.QtWidgets import QWidget
 
 
-def test_me(make_napari_viewer_proxy) -> None:
+def test_widget_creation(make_napari_viewer_proxy) -> None:
     viewer = make_napari_viewer_proxy()
     assert isinstance(GroupLayerWidget(viewer), QWidget)
