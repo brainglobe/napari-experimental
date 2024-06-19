43dd4615a55dcca6558c407b0dd5d206dadd24db
willGraham01 <ccaegra@ucl.ac.uk>
2024-06-19 14:11:35 +0100

    Create custom delegate and link to our viewer. Allow it to update layer information.

--- src/napari_experimental/group_layer_qt.py
+++ src/napari_experimental/group_layer_qt.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from napari._qt.containers import QtNodeTreeModel, QtNodeTreeView
+from qtpy.QtCore import QModelIndex
+from qtpy.QtWidgets import QStyledItemDelegate, QWidget
 
-from napari_experimental.group_layer import GroupLayer
+from napari_experimental.group_layer import GroupLayer, NodeWrappingLayer
 
 if TYPE_CHECKING:
     from qtpy.QtWidgets import QWidget
@@ -17,6 +19,19 @@ class QtGroupLayerModel(QtNodeTreeModel[GroupLayer]):
         self.setRoot(root)
 
 
+class QtGroupLayerDelegate(QStyledItemDelegate):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def setModelData(
+        self, editor: QWidget, model: QtGroupLayerModel, index: QModelIndex
+    ) -> None:
+        item_to_edit = model.getItem(index)
+        if isinstance(item_to_edit, NodeWrappingLayer) and editor.text():
+            item_to_edit.layer.name = editor.text()
+
+
 class QtGroupLayerView(QtNodeTreeView):
     _root: GroupLayer
     model_class = QtGroupLayerModel
@@ -24,3 +39,5 @@ class QtGroupLayerView(QtNodeTreeView):
     def __init__(self, root: GroupLayer, parent: QWidget = None):
         super().__init__(root, parent)
         self.setRoot(root)
+
+        self.setItemDelegate(QtGroupLayerDelegate())
