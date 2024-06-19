e66680b90568a7f42adf66ab90e3d040b242c37b
willGraham01 <ccaegra@ucl.ac.uk>
2024-06-19 15:59:34 +0100

    Typehints don't carry through fixtures

--- tests/test_qt_group_layer_model.py
+++ tests/test_qt_group_layer_model.py
@@ -8,6 +8,6 @@ def group_layer_data(points_layer, image_layer) -> GroupLayer:
     return GroupLayer([points_layer, image_layer])
 
 
-def test_qt_group_layer_model(group_layer_data, qtmodeltester):
+def test_qt_group_layer_model(group_layer_data: GroupLayer, qtmodeltester):
     model = QtGroupLayerModel(root=group_layer_data)
     qtmodeltester.check(model)
