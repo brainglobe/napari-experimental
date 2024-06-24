af93747ea489840d6b1e7c2a712b6e058c2d8db1
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2024-06-24 14:12:08 +0100

    expand docstrings

--- src/napari_experimental/group_layer_controls.py
+++ src/napari_experimental/group_layer_controls.py
@@ -36,6 +36,25 @@ class QtGroupLayerControls(QFrame):
 
 
 class QtGroupLayerControlsContainer(QtLayerControlsContainer):
+    """Container for layer control widgets.
+
+    Parameters
+    ----------
+    viewer : napari.components.ViewerModel
+        Napari viewer
+    group_layers : GroupLayer
+        Current group layers
+
+    Attributes
+    ----------
+    empty_widget : qtpy.QtWidgets.QFrame
+        Empty placeholder frame for when no layer is selected.
+    viewer : napari.components.ViewerModel
+        Napari viewer containing the rendered scene, layers, and controls.
+    widgets : dict
+        Dictionary of key value pairs matching GroupLayer or GroupLayerNode
+        with their widget controls. widgets[item] = controls
+    """
 
     def __init__(
         self, viewer: "napari.viewer.Viewer", group_layers: GroupLayer
@@ -57,7 +76,7 @@ class QtGroupLayerControlsContainer(QtLayerControlsContainer):
         group_layers.events.removed.connect(self._remove)
         group_layers.selection.events.active.connect(self._display)
 
-    def _initialise_controls(self, group_layers):
+    def _initialise_controls(self, group_layers: GroupLayer) -> None:
         """Initialise controls for any items already present in group
         layers"""
         for item in group_layers:
@@ -66,7 +85,7 @@ class QtGroupLayerControlsContainer(QtLayerControlsContainer):
 
             self._add_item(item)
 
-    def _add(self, event: Event):
+    def _add(self, event: Event) -> None:
         """Add the controls target item to the list of control widgets.
 
         Parameters
@@ -77,7 +96,14 @@ class QtGroupLayerControlsContainer(QtLayerControlsContainer):
         item = event.value
         self._add_item(item)
 
-    def _add_item(self, item: GroupLayer | GroupLayerNode):
+    def _add_item(self, item: GroupLayer | GroupLayerNode) -> None:
+        """Add the controls target item to the list of control widgets.
+
+        Parameters
+        ----------
+        item : GroupLayer or GroupLayerNode
+            Item to add control widget for.
+        """
         if item.is_group():
             controls = QtGroupLayerControls()
         else:
@@ -88,7 +114,7 @@ class QtGroupLayerControlsContainer(QtLayerControlsContainer):
         self.addWidget(controls)
         self.widgets[item] = controls
 
-    def _display(self, event: Event):
+    def _display(self, event: Event) -> None:
         """Change the displayed controls to be those of the target item.
 
         Parameters
@@ -99,14 +125,21 @@ class QtGroupLayerControlsContainer(QtLayerControlsContainer):
         item = event.value
         self._display_item(item)
 
-    def _display_item(self, item: GroupLayer | GroupLayerNode | None):
+    def _display_item(self, item: GroupLayer | GroupLayerNode | None) -> None:
+        """Change the displayed controls to be those of the target item.
+
+        Parameters
+        ----------
+        item : GroupLayer or GroupLayerNode
+            Item to display controls for.
+        """
         if item is None:
             self.setCurrentWidget(self.empty_widget)
         else:
             controls = self.widgets[item]
             self.setCurrentWidget(controls)
 
-    def _remove(self, event: Event):
+    def _remove(self, event: Event) -> None:
         """Remove the controls target item from the list of control widgets.
 
         Parameters
