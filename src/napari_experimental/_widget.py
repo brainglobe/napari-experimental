500565e029a5af54ee439c9d23ad559ac9dcbe22
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2024-06-25 12:38:37 +0100

    Adds independent layer controls to widget (#22)
    * add group layer controls
    
    * fix controls for latest changes to group layer
    
    * remove syncing of selection
    
    * add tests for group layer controls
    
    * fix TypeError
    
    * expand docstrings
    
    * react to sub-groups

--- src/napari_experimental/_widget.py
+++ src/napari_experimental/_widget.py
@@ -6,10 +6,12 @@ from typing import TYPE_CHECKING, Tuple
 from napari.components import LayerList
 from napari.layers import Layer
 from napari.utils.events import Event
-from qtpy.QtCore import QModelIndex
 from qtpy.QtWidgets import QPushButton, QVBoxLayout, QWidget
 
-from napari_experimental.group_layer import GroupLayer, GroupLayerNode
+from napari_experimental.group_layer import GroupLayer
+from napari_experimental.group_layer_controls import (
+    QtGroupLayerControlsContainer,
+)
 from napari_experimental.group_layer_qt import (
     QtGroupLayerView,
 )
@@ -33,11 +35,12 @@ class GroupLayerWidget(QWidget):
         self.group_layers_view = QtGroupLayerView(
             self.group_layers, parent=self
         )
+        self.group_layers_controls = QtGroupLayerControlsContainer(
+            self.viewer, self.group_layers
+        )
 
         self.global_layers.events.inserted.connect(self._new_layer)
         self.global_layers.events.removed.connect(self._removed_layer)
-
-        self.group_layers_view.clicked.connect(self._item_selected_in_view)
 
         self.add_group_button = QPushButton("Add empty layer group")
         self.add_group_button.clicked.connect(self._new_layer_group)
@@ -46,20 +49,10 @@ class GroupLayerWidget(QWidget):
         self.enter_debugger.clicked.connect(self._enter_debug)
 
         self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.group_layers_controls)
         self.layout().addWidget(self.enter_debugger)
         self.layout().addWidget(self.add_group_button)
         self.layout().addWidget(self.group_layers_view)
-
-    def _item_selected_in_view(self, selected_index: QModelIndex) -> None:
-        """
-        When the user clicks an item in the tree, this function runs
-        """
-        the_node = self.group_layers_view.model().getItem(selected_index)
-        if isinstance(the_node, GroupLayer):
-            pass
-        elif isinstance(the_node, GroupLayerNode):
-            the_layer: Layer = the_node.layer
-            self.global_layers.selection.select_only(the_layer)
 
     def _new_layer_group(self) -> None:
         """ """
